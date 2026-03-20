5a486f719860fdf5a5474e9a545506729a93e0ce
serhioj <goryaynov_serzh@mail.ru>
2026-03-20 18:51:12 +0300

    fix: заменить маршрут /dashboard на редирект на /admin

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,8 @@ Route::get('/', function () {
     return redirect('/admin');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::redirect('/welcome', '/admin');
+Route::redirect('/dashboard', '/admin');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
